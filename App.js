56792f6ca91c3139b60545f0b039516ebb4252ac
baesumin <qotnals12345@naver.com>
2021-09-19 13:54:12 +0900

    Add TextInput

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput value="baesumin" />
       <Text>Welcome to this CI/CD course!</Text>
       <StatusBar style="auto" />
     </View>
