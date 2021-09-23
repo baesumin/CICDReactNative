bd79452a891dc456b1080533badd800f949109c4
baesumin <qotnals12345@naver.com>
2021-09-23 12:19:28 +0900

    feat-3

--- components/Greeting.js
+++ components/Greeting.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
+import {
+  generateTestCrash,
+  hasCrashedInLastSession,
+  lastSessionCrashReport
+} from 'appcenter-crashes';
+import { trackEvent } from 'appcenter-analytics';
 
 export default function Greeting() {
+  useEffect(() => {
+    checkPreviousSession();
+  }, []);
+
+  const checkPreviousSession = async () => {
+    const didCrash = await hasCrashedInLastSession();
+    if (didCrash) {
+      const report = await lastSessionCrashReport();
+      Alert.alert('Sorry');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TextInput value="baesumin" />
-      <Text>Welcome to this CI/CD course!</Text>
+      <Button
+        title="Crashes"
+        onPress={() =>
+          trackEvent('calcuate_inflation', {
+            Internet: 'WiFi',
+            GPS: 'off'
+          })
+        }
+      />
+      <Text>Welcome to this CI/CD course!(feauture-3)</Text>
       <StatusBar style="auto" />
     </View>
   );
